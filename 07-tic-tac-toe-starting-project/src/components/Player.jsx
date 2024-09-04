c702c3e7207497206be6d1a3972e128bc6022e05
darthr4v3m <3935459+darthr4v3m@users.noreply.github.com>
2024-09-04 22:00:52 +0200

    feat:

---
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleClick() {
+    setIsEditing((editing) => !editing);
+  }
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {isEditing ? <input type="text" required value={playerName} onChange={handleChange}/> : <span className="player-name">{playerName}</span>}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
