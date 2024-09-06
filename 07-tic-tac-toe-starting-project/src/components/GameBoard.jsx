328eb5c15d21dd6c639da063fad859f03021e18b
darthr4v3m <3935459+darthr4v3m@users.noreply.github.com>
2024-09-06 22:39:09 +0200

    feat: add logic to alternate players

--- 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -6,15 +6,16 @@ const initialGameBoard = [
     [null, null, null]
 ];
 
-export default function GameBoard() {
+export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
     const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
     function handleSelectSquare(rowIndex, colIndex) {
         setGameBoard((previousGameBoard) => {
             const updateBoard = [...previousGameBoard.map(innerArray => [...innerArray])];
-            updateBoard[rowIndex][colIndex] = 'X';
+            updateBoard[rowIndex][colIndex] = activePlayerSymbol;
             return updateBoard;
         });
+        onSelectSquare();
     }
 
     return (
