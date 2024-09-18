3fdff62171c046038283edeb6df5a004c96ca114
darthr4v3m <3935459+darthr4v3m@users.noreply.github.com>
2024-09-18 21:32:34 +0200

    style: :lipstick: dynamic/conditional styling

--- 07-styling-react-components/src/components/AuthInputs.jsx
+++ 07-styling-react-components/src/components/AuthInputs.jsx
@@ -15,16 +15,16 @@ const Label = styled.label`
   font-weight: 700;
   letter-spacing: 0.1em;
   text-transform: uppercase;
-  color: #6b7280;
+  color: ${(props) => props.$invalid ? '#f87171' : '#6b7280'};
 `
 
 const Input = styled.input`
   width: 100%;
   padding: 0.75rem 1rem;
   line-height: 1.5;
-  background-color: #d1d5db;
-  color: #374151;
-  border: 1px solid transparent;
+  background-color: ${(props) => props.$invalid ? '#fed2d2' : '#d1d5db'};
+  color: ${(props) => props.$invalid ? '#ef4444' : '#374151'};
+  border: 1px solid ${(props) => props.$invalid ? '#f73f3f' : 'transparent'};
   border-radius: 0.25rem;
   box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
 `
@@ -53,16 +53,21 @@ export default function AuthInputs() {
     <div id="auth-inputs">
       <ControlContainer >
         <p>
-          <Label className={emailNotValid ? 'invalid' : undefined}>Email</Label>
+          <Label $invalid={emailNotValid}>
+            Email
+          </Label>
           <Input
+            $invalid={emailNotValid}
             type="email"
-            className={emailNotValid ? 'invalid' : undefined}
             onChange={(event) => handleInputChange('email', event.target.value)}
           />
         </p>
         <p>
-          <Label className={emailNotValid ? 'invalid' : undefined}>Password</Label>
+          <Label $invalid={passwordNotValid}>
+            Password
+          </Label>
           <Input
+            $invalid={passwordNotValid}
             type="password"
             className={passwordNotValid ? 'invalid' : undefined}
             onChange={(event) =>
