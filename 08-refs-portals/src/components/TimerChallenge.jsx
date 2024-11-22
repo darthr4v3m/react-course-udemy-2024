d72cd511bc429380cc7588d00defe62d328db1cf
darthr4v3m <3935459+darthr4v3m@users.noreply.github.com>
2024-11-22 22:59:19 +0100

    feat(refs): sharing state across components
    Refers to lecture 8-143

--- 08-refs-portals/src/components/TimerChallenge.jsx
+++ 08-refs-portals/src/components/TimerChallenge.jsx
@@ -11,8 +11,11 @@ export default function TimerChallenge({ title, targetTime }) {
 
     if (timeRemaining <= 0) {
         clearInterval(timer.current);
-        setTimeRemaining(targetTime * 1000);
         resultDialog.current.open();
+    }
+
+    function handleReset() {
+        setTimeRemaining(targetTime * 1000);
     }
 
     function handleStart() {
@@ -29,7 +32,12 @@ export default function TimerChallenge({ title, targetTime }) {
 
     return (
         <>
-            <ResultModal ref={resultDialog} targetTime={targetTime} result="lose!" />
+            <ResultModal
+                ref={resultDialog}
+                targetTime={targetTime}
+                remainingTime={timeRemaining}
+                onReset={handleReset}
+            />
             <section className="challenge">
                 <h2>{title}</h2>
                 <p className="challenge-time">
