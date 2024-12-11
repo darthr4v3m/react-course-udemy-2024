ab9c83196485ddda04281ac54c62e5d1ade4d3ce
darthr4v3m <3935459+darthr4v3m@users.noreply.github.com>
2024-12-11 22:44:54 +0100

    feat: create skeleton fo sidebar

---
+++ 09-project-managment-app/ProjectSidebarComponent.jsx
@@ -0,0 +1,10 @@
+export default function ProjectsSidebar() {
+    return <aside>
+        <h2>Your projects</h2>
+        <div>
+            <button>
+                + Add project
+            </button>
+        </div>
+    </aside>
+}
